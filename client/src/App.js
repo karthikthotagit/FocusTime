0230664e3139da752bbcb565c9e3eb00d5e8fe51
Karthik Thota <karthikthota@MacBookPro.lan>
2025-11-04 23:01:18 -0600

    Project Checkpoint-1: Basic Setup and Initial Implementation

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FocusTime App</h1>
+      <p>A structured study timer for students with ADHD</p>
+      <button>Start Timer</button>
+    </div>
+  );
+}
+
+export default App;
+
