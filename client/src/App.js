3f4e58b60c515b723a3919fd328bfa70e9c441da
Karthik Thota <karthikthota@MacBookPro.lan>
2025-11-20 23:17:12 -0600

    checkpoint 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
-import logo from './logo.svg';
 import React from "react";
-import "./App.css";
 
 function App() {
+  const startSession = async () => {
+    const res = await fetch("http://localhost:5001/api/start-session", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ duration: 25 })
+    });
+
+    const data = await res.json();
+    console.log("Session created:", data);
+  };
+
   return (
     <div className="App">
       <h1>FocusTime App</h1>
-      <p>A structured study timer for students with ADHD</p>
-      <button>Start Timer</button>
+      <p>A structured study timer</p>
+      <button onClick={startSession}>Start Timer</button>
     </div>
   );
 }
 
 export default App;
-
--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
-const express = require("express");
-const app = express();
-const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Backend running!");
-});
+const express = require("express");
+const timerController = require("./controllers/timerController");
+
+const app = express();
+const PORT = 5001;
+
+app.use(express.json());
+
+app.post("/api/start-session", (req, res) => timerController.start(req, res));
+
+app.get("/", (req, res) => res.send("Backend running with architecture!"));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
